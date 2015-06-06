9272a90035d7b971fb7c2ab09fe561d8be3f4a6d
mambax7 <mambax7@gmail.com>
2015-06-06 07:44:25 -0400

    Original files

---
+++ ixt01102_v2/xo-info.php
@@ -0,0 +1,34 @@
+<?php
+/*
+* Qualitative Themes for XOOPS
+*
+* @license       http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+* @project       IXThemes Project, http://ixthemes.org
+* @theme_name    ixt01102
+* @theme_demo    http://ixthemes.sourceforge.net/demo/x/index.php?xoops_theme_select=ixt01102
+* @theme_author  Alexander Galochkin (algalochkin at users.sourceforge.net)
+* @version       $Id: xo-info.php 1538 2009-11-29 12:47:46Z algalochkin $
+*
+* Thanks you, that you have left this header untouched!!!
+*/
+return array(
+ "languages"   => array("main"),
+ "copyright"   => "&copy; Created by IXThemes <http://ixthemes.org>",
+ "name"        => "ixt01102",
+ "description" => "XOOPS 2.0.1x, 2.3.x and 2.4.x, widgets, valid XHTML",
+ "version"     => "1.0",
+ "format"      => "XOOPS",
+ "author"      => "IXThemes Project",
+ "demo"        => "http://xoops240demo.ixthemes.org/index.php?xoops_theme_select=ixt01102",
+ "demo1"       => "http://xoops233demo.ixthemes.org/index.php?xoops_theme_select=ixt01102",
+ "demo2"       => "http://ixthemes.sourceforge.net/demo/x/index.php?xoops_theme_select=ixt01102",
+ "demo3"        => "http://xoops241demo.ixthemes.org/index.php?xoops_theme_select=ixt01102",
+ "demo4"        => "http://xoops242demo.ixthemes.org/index.php?xoops_theme_select=ixt01102",
+ "url"         => "http://ixthemes.org",
+ "download"    => "http://ixthemes.com",
+ "W3C"         => "XHTML 1.0 Transitional - CSS 1/2/3",
+ "licence"     => "GPL",
+ "thumbnail"   => "shot.gif",
+ "screenshot"  => "screenshot.jpg",
+);
+?>
